69cd0475053046e3f5a110a10d81f11809ef9cda
taylorna <72960523+taylorna@users.noreply.github.com>
2020-10-20 10:38:00 -0700

    Update PhiqEval.m
    Added a short clarification statement and fixed a typo

--- Newton_Raphson/OG/PhiqEval.m
+++ Newton_Raphson/OG/PhiqEval.m
@@ -1,5 +1,6 @@
 function Phiq=PhiqEval(q)
-% Evalyation of Phiq(q)=partial derevative matrix of Phi with respect to q
+% Evaluation of Phiq(q)=partial derevative matrix of Phi with respect to q
+% This is where the Jacobian must be defined. Each term must be calculated outide of matlab.
 
 Phiq=Phiq(q);
 
